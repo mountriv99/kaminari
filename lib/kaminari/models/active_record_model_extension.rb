04f447c3f09cfd382e74b935dd0f0c2023fe8ece
Eric <eric@ubuntu.(none)>
2013-10-01 12:22:48 +0800

    allows a :total_count option to be passed to the page method so that cached counters can be used

--- lib/kaminari/models/active_record_model_extension.rb
+++ lib/kaminari/models/active_record_model_extension.rb
@@ -9,8 +9,10 @@ module Kaminari
 
       # Fetch the values at the specified page number
       #   Model.page(5)
-      self.scope Kaminari.config.page_method_name, Proc.new {|num|
-        limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
+      self.scope Kaminari.config.page_method_name, Proc.new {|num, options = {}|
+        r = limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
+        r.instance_variable_set(:@total_count, options[:total_count]) if options[:total_count]
+        r
       } do
         include Kaminari::ActiveRecordRelationMethods
         include Kaminari::PageScopeMethods
